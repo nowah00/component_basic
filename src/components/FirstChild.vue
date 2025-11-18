063ba2f5b3cb5ea0a95ef0ff5c51ce1cad190b1a
nowah00 <hawon2k@gmail.com>
2025-11-18 15:25:49 +0900

    Second Commit

--- src/components/FirstChild.vue
+++ src/components/FirstChild.vue
@@ -1,12 +1,13 @@
+<script>
+export default {
+  beforeUnmount() {
+    console.log('beforeUnmount');
+  },
+  unmounted() {
+    console.log('unmouted');
+  },
+};
+</script>
 <template>
-  <h1>컴포넌트</h1>
-  <p>첫번째 만든 컴포넌트</p>
+  <h1>First Child</h1>
 </template>
-<style>
-h1 {
-  color: blue;
-}
-p {
-  color: green;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import SecondChild from './components/SecondChild.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 
-// 앱 인스턴스를 변수에 담고
-const app = createApp(App);
-
-// 여기에 전역 컴포넌트 등록
-app.component('SecondChild', SecondChild);
-
-app.mount('#app');
+createApp(App).mount("#app");
